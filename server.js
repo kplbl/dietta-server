d21d8b3ee630465cd3d4b0079b8ee5ac4da29e39
kplbl <51096422+kplbl@users.noreply.github.com>
2022-01-24 10:02:14 +0100

    cors allow all

--- server.js
+++ server.js
@@ -9,7 +9,11 @@ const PORT = process.env.PORT || 5000;
 const PASS = process.env.DB_PASS;
 
 app.use(helmet());
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 
 const baza = `mongodb+srv://blaz123:${PASS}@cluster0.opr9q.mongodb.net/dietta?retryWrites=true&w=majority`;
